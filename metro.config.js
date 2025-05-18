1635575f25bde68d5782d84e870c82e02c23ae2b
NawalSultan1 <nawalsultan2001@gmail.com>
2025-05-19 01:34:09 +0500

    metro config updated (removed mergeConfig)

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,18 @@
-const { getDefaultConfig, mergeConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(defaultConfig, {
-  resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts, 'tflite'],
-  },
-  transformer: {
-    ...defaultConfig.transformer,
-    unstable_allowRequireContext: true,
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-});
+config.resolver.assetExts.push('tflite');
+config.transformer = {
+     ...config.transformer,
+     unstable_allowRequireContext: true,
+     getTransformOptions: async () => ({
+       transform: {
+         experimentalImportSupport: false,
+         inlineRequires: true,
+       },
+     }),
+   };
+   
+
+module.exports = config;
